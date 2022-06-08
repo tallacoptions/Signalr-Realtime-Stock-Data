c6f90351cf65688f77002d52c0e47367b4b5a5c8
ndaopetit <mamadou.ndao@tallacoptions.com>
2022-06-08 06:02:34 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.AspNetCore.SignalR.Client;
+
+using RealtimeStockDataUsingSignalr.Models;
+
+namespace RealtimeStockDataUsingSignalr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly HubConnection _connection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Create list of stocks
+            Stocks = new ObservableCollection<Stock>
+            {
+                new Stock("GOOG"),
+                new Stock("AAPL"),
+                new Stock("FB"),
+                new Stock("AMZN"),
+                new Stock("MSFT"),
+                new Stock("TSLA"),
+                new Stock("TWTR"),
+                new Stock("INTC"),
+                new Stock("PFE"),
+                new Stock("SNAP")
+            };
+
+            DgStocks.ItemsSource = Stocks;
+
+            //Create hub connection
+            _connection = new HubConnectionBuilder()
+                .WithUrl("https://tallacoptions.com/livehub")
+                .Build();
+            //Register NewData handler with the server
+            _connection.On<StockData>(nameof(NewData), NewData);
+        }
+
+        private ObservableCollection<Stock> Stocks { get; }
+
+        private async void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            await _connection.StartAsync();
+            if (_connection.State == HubConnectionState.Connected)
+            {
+                //Register each stock with the server
+                foreach (var s in Stocks)
+                {
+                    await _connection.InvokeAsync("JoinGroup", s.Ticker);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Could not connect to the server.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void BtnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            await _connection.StopAsync();
+        }
+
+        private Task NewData(StockData quote)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(quote.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.Last = quote.Last;
+                stock.LastSize = quote.LastSize;
+                stock.Bid = quote.Bid;
+                stock.BidSize = quote.BidSize;
+                stock.Ask = quote.Ask;
+                stock.AskSize = quote.AskSize;
+                stock.Volume = quote.Volume;
+                stock.Open = quote.Open;
+                stock.High = quote.High;
+                stock.Low = quote.Low;
+            }            
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtimeStockDataUsingSignalr.Models
+{
+    public class Stock
+    {
+        private string _last, _lastSize, _bid, _bidSize, _ask, _askSize, _volume, _open, _high, _low;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Stock(string ticker)
+        {
+            Ticker = ticker;
+        }
+
+        public string Ticker { get; }
+        public string Last
+        {
+            get => _last;
+            set
+            {
+                if (value != _last)
+                {
+                    _last = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string LastSize
+        {
+            get => _lastSize;
+            set
+            {
+                if (value != _lastSize)
+                {
+                    _lastSize = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string Bid
+        {
+            get => _bid;
+            set
+            {
+                if (value != _bid)
+                {
+                    _bid = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string BidSize
+        {
+            get => _bidSize;
+            set
+            {
+                if (value != _bidSize)
+                {
+                    _bidSize = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string Ask
+        {
+            get => _ask;
+            set
+            {
+                if (value != _ask)
+                {
+                    _ask = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string AskSize
+        {
+            get => _askSize;
+            set
+            {
+                if (value != _askSize)
+                {
+                    _askSize = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string Volume
+        {
+            get => _volume;
+            set
+            {
+                if (value != _volume)
+                {
+                    _volume = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string Open
+        {
+            get => _open;
+            set
+            {
+                if (value != _open)
+                {
+                    _open = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string High
+        {
+            get => _high;
+            set
+            {
+                if (value != _high)
+                {
+                    _high = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string Low
+        {
+            get => _low;
+            set
+            {
+                if (value != _low)
+                {
+                    _low = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
