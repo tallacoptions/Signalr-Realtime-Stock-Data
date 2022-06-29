199d337894bf5534d57966852079dab3d46a60b0
ndaopetit <mamadou.ndao@tallacoptions.com>
2022-06-29 05:53:23 +0200

    Add quote DTOs.

---
+++ Models/QuoteLast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtimeStockDataUsingSignalr.Models
+{
+    public class QuoteLast
+    {
+        public string Ticker { get; set; }
+        public string Last { get; set; }
+        public string DayNetChange { get; set; }
+        public string DayPercChange { get; set; }
+    }
+}
