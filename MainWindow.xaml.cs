89cf71e80d6289c72540c66cd4b1a7aef387e9b7
ndaopetit <mamadou.ndao@tallacoptions.com>
2022-06-29 05:54:46 +0200

    Adapt handlers.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -52,8 +52,17 @@ namespace RealtimeStockDataUsingSignalr
             _connection = new HubConnectionBuilder()
                 .WithUrl("https://tallacoptions.com/livehub")
                 .Build();
-            //Register NewData handler with the server
-            _connection.On<StockData>(nameof(NewData), NewData);
+            //Register handlers with the server
+            _connection.On<QuoteItem>("Open", UpdateOpen);
+            _connection.On<QuoteItem>("High", UpdateHigh);
+            _connection.On<QuoteItem>("Low", UpdateLow);
+            _connection.On<QuoteItem>("Volume", UpdateVolume);
+            _connection.On<QuoteLast>("Last", UpdateLast);
+            _connection.On<QuoteItem>("LastSize", UpdateLastSize);
+            _connection.On<QuoteItem>("Bid", UpdateBid);
+            _connection.On<QuoteItem>("BidSize", UpdateBidSize);
+            _connection.On<QuoteItem>("Ask", UpdateAsk);
+            _connection.On<QuoteItem>("AskSize", UpdateAskSize);
         }
 
         private ObservableCollection<Stock> Stocks { get; }
@@ -63,10 +72,19 @@ namespace RealtimeStockDataUsingSignalr
             await _connection.StartAsync();
             if (_connection.State == HubConnectionState.Connected)
             {
-                //Register each stock with the server
+                //Subscribe to data feed
                 foreach (var s in Stocks)
                 {
-                    await _connection.InvokeAsync("JoinGroup", s.Ticker);
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-Open");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-High");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-Low");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-Volume");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-Last");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-LastSize");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-Bid");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-BidSize");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-Ask");
+                    await _connection.InvokeAsync("JoinGroup", $"{s.Ticker}-AskSize");
                 }
             }
             else
@@ -79,23 +97,156 @@ namespace RealtimeStockDataUsingSignalr
         {
             await _connection.StopAsync();
         }
-
-        private Task NewData(StockData quote)
+        
+        /// <summary>
+        /// Updates Open field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateOpen(QuoteItem item)
         {
-            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(quote.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if(stock != null)
+            {
+                stock.Open = item.Data;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates High field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateHigh(QuoteItem item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
             if (stock != null)
             {
-                stock.Last = quote.Last;
-                stock.LastSize = quote.LastSize;
-                stock.Bid = quote.Bid;
-                stock.BidSize = quote.BidSize;
-                stock.Ask = quote.Ask;
-                stock.AskSize = quote.AskSize;
-                stock.Volume = quote.Volume;
-                stock.Open = quote.Open;
-                stock.High = quote.High;
-                stock.Low = quote.Low;
-            }            
+                stock.High = item.Data;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates Low field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateLow(QuoteItem item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.Low = item.Data;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates Volume field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateVolume(QuoteItem item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.Volume = item.Data;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates Last field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateLast(QuoteLast item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.Last = item.Last;
+                stock.NetChange = item.DayNetChange;
+                stock.PercChange = item.DayPercChange;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates LastSize field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateLastSize(QuoteItem item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.LastSize = item.Data;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates Bid field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateBid(QuoteItem item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.Bid = item.Data;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates BidSize field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateBidSize(QuoteItem item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.BidSize = item.Data;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates Ask field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateAsk(QuoteItem item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.Ask = item.Data;
+            }
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Updates AskSize field.
+        /// </summary>
+        /// <param name="item">Quote data.</param>
+        /// <returns></returns>
+        private Task UpdateAskSize(QuoteItem item)
+        {
+            var stock = Stocks.FirstOrDefault(s => s.Ticker.Equals(item.Ticker, StringComparison.InvariantCultureIgnoreCase));
+            if (stock != null)
+            {
+                stock.AskSize = item.Data;
+            }
             return Task.CompletedTask;
         }
     }
